8d7a377f75a3d7807da8068bb8d2e5295ef410d0
Justin Perez <justin@startrev.com>
2020-07-28 02:47:03 -0700

    Update

---
+++ src/pages/success.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h2>You dropped me a new line</h2>
+    <h4> Congrats,i&#39;ll be sure to check it out when I can and add a reply</h4>
+  </div>
+)
